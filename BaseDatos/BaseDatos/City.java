cc34575b861ef5c321d1a8b96544c61fdbe55397
Cesar-Sanchez-Herrera <57964628+Cesar-Sanchez-Herrera@users.noreply.github.com>
2019-12-10 17:55:22 -0600

    Add files via upload

---
+++ BaseDatos/BaseDatos/City.java
@@ -0,0 +1,31 @@
+package BaseDatos;
+
+public class City {
+
+	private String state_id;
+	private String description;
+	private String shipping_zone_id;
+	
+	public String getState_id() {
+		return state_id;
+	}
+	public void setState_id(String state_id) {
+		this.state_id = state_id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getShipping_zone_id() {
+		return shipping_zone_id;
+	}
+	public void setShipping_zone_id(String shipping_zone_id) {
+		this.shipping_zone_id = shipping_zone_id;
+	}
+	
+	
+	
+	
+}
